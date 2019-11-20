33039483645c3b3386eb5992534ce6cf92765960
Matt Woelk <matt@mattwoelk.ca>
2019-11-19 20:20:45 -0600

    Refactoring, for much clarity.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 extern crate discord;
 
+use discord::model::ChannelId;
 use discord::model::Event;
+use discord::model::Message;
 use discord::model::MessageId;
+use discord::model::Reaction;
 use discord::Discord;
 
 use color_backtrace;
 
 #[derive(Default)]
 struct Game {
+    channel_id: Option<ChannelId>,
     most_recent_message_id: Option<MessageId>,
 }
 
@@ -29,31 +33,10 @@ fn main() {
     loop {
         match connection.recv_event() {
             Ok(Event::MessageCreate(message)) => {
-                println!("{} says: {}", message.author.name, message.content);
-                if message.content == "!test" {
-                    let sent_message = discord.send_message(
-                        message.channel_id,
-                        "This is a reply to the test.",
-                        "",
-                        false,
-                    );
-
-                    game.most_recent_message_id = sent_message.ok().map(|a| a.id);
-                } else if message.content == "!quit" {
-                    println!("Quitting.");
-                    break;
-                }
+                handle_user_message(&message, &discord, &mut game);
             }
-            Ok(Event::ReactionAdd(event)) => {
-                if let Some(most_recent_message_id) = game.most_recent_message_id {
-                    if event.message_id == most_recent_message_id {
-                        dbg!("MOST RECENT");
-                    }
-                }
-                dbg!(event.user_id);
-                dbg!(event.channel_id);
-                dbg!(event.message_id);
-                dbg!(event.emoji);
+            Ok(Event::ReactionAdd(reaction)) => {
+                handle_user_reaction(&reaction, &discord, &mut game);
             }
             Ok(_) => {}
             Err(discord::Error::Closed(code, body)) => {
@@ -63,4 +46,38 @@ fn main() {
             Err(err) => println!("Receive error: {:?}", err),
         }
     }
+}
+
+fn handle_user_message(message: &Message, discord: &Discord, game: &mut Game) {
+    println!("{} says: {}", message.author.name, message.content);
+
+    match message.content.as_str() {
+        "!play" => {
+            dbg!("start play!");
+            game.channel_id = Some(message.channel_id);
+        }
+        "!test" => {
+            let sent_message = discord.send_message(
+                message.channel_id,
+                "This is a reply to the test.",
+                "",
+                false,
+            );
+
+            game.most_recent_message_id = sent_message.ok().map(|a| a.id);
+        }
+        _ => {}
+    }
+}
+
+fn handle_user_reaction(reaction: &Reaction, discord: &Discord, game: &mut Game) {
+    if let Some(most_recent_message_id) = game.most_recent_message_id {
+        if reaction.message_id == most_recent_message_id {
+            dbg!("MOST RECENT");
+        }
+    }
+    dbg!(&reaction.user_id);
+    dbg!(&reaction.channel_id);
+    dbg!(&reaction.message_id);
+    dbg!(&reaction.emoji);
 }
